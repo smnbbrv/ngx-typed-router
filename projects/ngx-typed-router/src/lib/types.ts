e1e50a98ef61c5dcc3b54a8ce51ea2f8cd49a4ad
smnbbrv <simonellez@gmail.com>
2018-11-26 11:18:03 +0100

    fix: extend original router's data and params types to be used in resolve services

--- projects/ngx-typed-router/src/lib/types.ts
+++ projects/ngx-typed-router/src/lib/types.ts
@@ -1,13 +1,15 @@
 import { ActivatedRoute, ActivatedRouteSnapshot, Data, Params, Resolve } from '@angular/router';
 import { Observable } from 'rxjs';
 
-export interface TypedRouteSnapshot<D = Data, P = Params, Q = Params> extends ActivatedRouteSnapshot {
+export interface TypedRouteSnapshot<D extends Data = Data, P extends Params = Params, Q extends Params = Params>
+  extends ActivatedRouteSnapshot {
   params: P;
   queryParams: Q;
   data: D;
 }
 
-export interface TypedRoute<D = Data, P = Params, Q = Params> extends ActivatedRoute {
+export interface TypedRoute<D extends Data = Data, P extends Params = Params, Q extends Params = Params>
+  extends ActivatedRoute {
   params: Observable<P>;
   queryParams: Observable<Q>;
   data: Observable<D>;
@@ -16,6 +18,6 @@ export interface TypedRoute<D = Data, P = Params, Q = Params> extends ActivatedR
 
 export type ResolveConfig<T> = {
   [P in keyof T]: {
-    new (...args): Resolve<T[P]>;
+    new(...args): Resolve<T[P]>;
   };
 };
